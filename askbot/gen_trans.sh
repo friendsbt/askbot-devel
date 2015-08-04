3c85b9289c28baa2660103a30fd669f6622ac45c
tianran <tianran@fbt.com>
2015-08-04 17:42:33 +0800

    no answer trans Ok

--- askbot/gen_trans.sh
+++ askbot/gen_trans.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 python ~/Dev/Python/Envs/fbtask/fbtask/manage.py jinja2_makemessages -l zh_CN
+#python /home/tian-ran/FBT/fbt_askbot/manage.py jinja2_makemessages -l zh_CN
 django-admin.py compilemessages -l zh_CN
